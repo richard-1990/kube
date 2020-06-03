d7a3243d9f5c56eabaca74cd18641ccaf80e0bb2
Richard Price <richard.price@littlestarmedia.com>
2020-06-03 12:29:35 +0100

    base

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const http = require("http");
 
 const handleRequest = (request, response) => {
   console.log("Received request for URL: " + request.url);
-  console.log(process.env);
   response.writeHead(200);
-  response.end("a whole new builddddd");
+  response.end("Hello World");
 };
 var app = http.createServer(handleRequest);
 app.listen(9000);
